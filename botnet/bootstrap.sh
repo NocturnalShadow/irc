93dc9041057bf6d267ad41651fc5151d9db102c6
NocturnalShadow <bogdanscloud@gmail.com>
2022-05-11 17:33:49 +0300

    Changed repo url

--- botnet/bootstrap.sh
+++ botnet/bootstrap.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get install -y git-core libevent-dev python-dev python-setuptools
 easy_install pip
 pip install gevent
-git clone https://github.com/coleifer/irc.git /root/irc
+git clone https://github.com/NocturnalShadow/irc.git /root/irc
 cd /root/irc/
 python setup.py install
 cd /root/irc/botnet/
