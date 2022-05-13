b307791561c709cf6eaae5c0c684034c100f390c
NocturnalShadow <bogdanscloud@gmail.com>
2022-05-13 13:59:02 +0300

    Change bootstrap-boss.sh

--- botnet/bootstrap-boss.sh
+++ botnet/bootstrap-boss.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-IPV4=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+#IPV4=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+IPV4=localhost
 
 sudo apt-get install -y libevent-dev python-dev-is-python3 python-setuptools
 pip install -r requirements.txt
-python -m pip install -e .
+sudo python -m pip install -e .
 python ./botnet/boss.py -s $IPV4 -p 6677 -n boss -x auth-boss-now -c "#bots" -v 2
