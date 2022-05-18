871fac83c7b54f1a6b57ec5c385be4ab78be22a0
NocturnalShadow <bogdanscloud@gmail.com>
2022-05-18 15:44:12 +0300

    Adjust scripts

--- botnet/bootstrap-worker.sh
+++ botnet/bootstrap-worker.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install -y git-core libevent-dev python-dev-is-python3 python-setuptools python3-pip
-git clone https://github.com/NocturnalShadow/irc.git ~/irc
-cd ~/irc
-pip install -r requirements.txt
-python -m pip install -e .
-./botnet/worker.py %(worker_options)s > worker.log 2>&1
+git clone https://github.com/NocturnalShadow/irc.git ./irc
+pip install -r ./irc/requirements.txt
+python -m pip install -e ./irc
+./irc/botnet/worker.py %(worker_options)s > worker.log 2>&1
